4a830e177dd56c50f00162e5f1263d794c881837
Andrew1995bono <andrew.matseliukh@gmail.com>
2021-12-08 16:25:25 +0200

    active class toggle was added, added some styling to footer icons

--- src/app/static-components/header/header.component.ts
+++ src/app/static-components/header/header.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public tab : string = 'tab1';
+  public tab1 : string = '';
+  public tab2 : string = '';
+  public tab3 : string = '';
+  public tab4 : string = '';
+  public tab5 : string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onClick(number: number):void {
+    switch (number) {
+      case 1:
+        this.tab = 'tab1';
+        break;
+      case 2:
+        this.tab = 'tab2';
+        break;
+      case 3:
+        this.tab = 'tab3';
+        break;
+      case 4:
+        this.tab = 'tab4';
+        break;
+      case 5:
+        this.tab = 'tab5';
+        break;
+    }
+  }
 }
